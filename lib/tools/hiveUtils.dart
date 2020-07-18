af525c33891c117f7ce324f028d8c97ef3e59841
piecubed <40523150+piecubed@users.noreply.github.com>
2020-07-18 12:28:23 -0400

    Move utils to tools

---
+++ lib/tools/hiveUtils.dart
@@ -0,0 +1,14 @@
+import '../toro_server.dart';
+
+
+class HiveUtils {
+  static LazyBox<User> users;
+
+  static Box<Stock> stocks;
+
+  static LazyBox<Map<String, double>> history;
+
+  static Box<List<String>> watchedStocks;
+
+  static Box<String> usernames;
+}
---
+++ lib/tools/searchUtils.dart
@@ -0,0 +1,19 @@
+import 'package:fuzzy/fuzzy.dart';
+
+import '../toro_server.dart';
+
+
+class Search {
+  factory Search() => _cache;
+
+  Search._();
+
+  Future<void> init() async {
+    users.list.addAll(HiveUtils.usernames.toMap().keys.toList());
+    stocks.list.addAll(HiveUtils.stocks.toMap().keys.toList());
+  }
+
+  static final _cache = Search._();
+  final users = Fuzzy([], options: FuzzyOptions(isCaseSensitive: true));
+  final stocks = Fuzzy([], options: FuzzyOptions(isCaseSensitive: false));
+}
